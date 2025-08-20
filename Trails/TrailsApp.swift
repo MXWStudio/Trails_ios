c33f71ff226b3d881d1655f9518d36b5b2cd9258
MengXW <mxwfilm@gmail.com>
2025-08-20 18:26:58 +0800

    Initial Commit

---
+++ Trails/TrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailsApp.swift
+//  Trails
+//
+//  Created by 孟祥伟 on 2025/8/20.
+//
+
+import SwiftUI
+
+@main
+struct TrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrailsTests/TrailsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrailsTests.swift
+//  TrailsTests
+//
+//  Created by 孟祥伟 on 2025/8/20.
+//
+
+import Testing
+@testable import Trails
+
+struct TrailsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
